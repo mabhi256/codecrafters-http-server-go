fb819d1edb5b19660999b9443d6730504dd4ef4e
Abhishek Mandingi <mabhi256@gmail.com>
2025-08-16 13:36:32 -0400

    add /user-agent endpoint

--- app/main.go
+++ app/main.go
@@ -5,14 +5,55 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-// var _ = net.Listen
-// var _ = os.Exit
+const CRLF = "\r\n"
 
-const CLRF = "\r\n"
+type HTTPRequest struct {
+	Method   string
+	Path     string
+	Protocol string // Is usually HTTP/1.1, but keep it for the sake of completeness
+	Headers  map[string]string
+}
+
+type HTTPResponse struct {
+	Status  HTTPStatus
+	Headers map[string]string
+	Body    string
+}
+
+type HTTPStatus int
+
+const (
+	StatusOK HTTPStatus = 200
+	// StatusCreated   HTTPStatus = 201
+	// StatusAccepted  HTTPStatus = 202
+	// StatusNoContent HTTPStatus = 204
+
+	StatusBadRequest HTTPStatus = 400
+	// StatusUnauthorized HTTPStatus = 401
+	// StatusForbidden    HTTPStatus = 403
+	StatusNotFound HTTPStatus = 404
+
+	StatusInternalServerError HTTPStatus = 500
+)
+
+func (s HTTPStatus) String() string {
+	switch s {
+	case StatusOK:
+		return "200 OK"
+	case StatusBadRequest:
+		return "400 Bad Request"
+	case StatusNotFound:
+		return "404 Not Found"
+	case StatusInternalServerError:
+		return "500 Internal Server Error"
+	default:
+		return fmt.Sprintf("Status %d", int(s))
+	}
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -20,58 +61,120 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	OKStatus := Status("200 OK")
-	NotFoundStatus := Status("404 Not Found")
-	PlainTextContent := ContentType("text/plain")
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
 
-	// for {
-	c, err := l.Accept()
+		go handleRequest(c)
+	}
+}
+
+func handleRequest(c net.Conn) {
+	defer c.Close()
+
+	req, err := parseRequest(c)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		response := &HTTPResponse{
+			Status:  StatusBadRequest,
+			Headers: make(map[string]string),
+		}
+
+		sendResponse(c, response)
+		return
 	}
 
-	reader := bufio.NewReader(c)
-	line, err := reader.ReadString('\n')
+	response := req.RouteRequest()
+	sendResponse(c, response)
+}
+
+func parseRequest(conn net.Conn) (*HTTPRequest, error) {
+	reader := bufio.NewReader(conn)
+
+	// Read request requestLine
+	requestLine, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
-		os.Exit(1)
+		return nil, err
 	}
 
-	req := strings.Fields(line)
-	target := req[1]
+	// Parse request line
+	parts := strings.Fields(requestLine)
+	if len(parts) < 3 {
+		return nil, fmt.Errorf("invalid request line")
+	}
 
-	paths := strings.Split(target, "/")
+	request := &HTTPRequest{
+		Method:   parts[0],
+		Path:     parts[1],
+		Protocol: parts[2],
+		Headers:  make(map[string]string),
+	}
+
+	// Parse headers
+	for {
+		lineRaw, err := reader.ReadString('\n')
+		line := strings.TrimSpace(lineRaw)
+		if err != nil || line == "" {
+			break
+		}
+
+		headerParts := strings.Split(line, ":")
+		if len(headerParts) == 2 {
+			key := strings.ToLower(headerParts[0])
+			value := strings.TrimSpace(headerParts[1])
+			request.Headers[key] = value
+		}
+	}
+
+	return request, nil
+}
+
+func sendResponse(conn net.Conn, res *HTTPResponse) {
+	// Status Line
+	response := fmt.Sprintf("HTTP/1.1 %s%s", res.Status, CRLF)
+
+	// Headers
+	res.Headers["content-length"] = strconv.Itoa(len(res.Body))
+	for key, value := range res.Headers {
+		response += fmt.Sprintf("%s: %s%s", key, value, CRLF)
+	}
+
+	// Body
+	response += CRLF + res.Body
+
+	conn.Write([]byte(response))
+}
+
+func (req *HTTPRequest) RouteRequest() *HTTPResponse {
+	path := strings.Trim(req.Path, "/") // Remove leading and trailing '/'
+	pathParts := strings.Split(path, "/")
 
 	switch {
-	case target == "/":
-		response := OKStatus + ContentLength(0) + CLRF
-		c.Write([]byte(response))
-	case paths[0] == "" && paths[1] == "echo" && len(paths) == 3:
-		pathParam := paths[2]
-		response := OKStatus +
-			PlainTextContent +
-			ContentLength(len(pathParam)) +
-			CLRF + pathParam
-		c.Write([]byte(response))
+	case req.Path == "/":
+		return &HTTPResponse{
+			Status:  StatusOK,
+			Headers: make(map[string]string),
+		}
+	case pathParts[0] == "echo" && len(pathParts) == 2:
+		return &HTTPResponse{
+			Status:  StatusOK,
+			Headers: map[string]string{"content-type": "text/plain"},
+			Body:    pathParts[1],
+		}
+	case pathParts[0] == "user-agent" && len(pathParts) == 1:
+		return &HTTPResponse{
+			Status:  StatusOK,
+			Headers: map[string]string{"content-type": "text/plain"},
+			Body:    req.Headers["user-agent"],
+		}
 	default:
-		response := NotFoundStatus + ContentLength(0) + CLRF
-		c.Write([]byte(response))
+		return &HTTPResponse{
+			Status:  StatusNotFound,
+			Headers: make(map[string]string),
+		}
 	}
-
-	// 	c.Close()
-	// }
-}
-
-func Status(status string) string {
-	return fmt.Sprintf("HTTP/1.1 %s%s", status, CLRF)
-}
-
-func ContentType(ctype string) string {
-	return fmt.Sprintf("Content-Type: %s%s", ctype, CLRF)
-}
-
-func ContentLength(length int) string {
-	return fmt.Sprintf("Content-Length: %v%s", length, CLRF)
 }
