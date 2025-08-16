18abe09868cb11b64228b71cb8a2b79993d2ca73
Abhishek Mandingi <mabhi256@gmail.com>
2025-08-16 11:05:14 -0400

    send echo response

--- app/main.go
+++ app/main.go
@@ -12,6 +12,8 @@ import (
 // var _ = net.Listen
 // var _ = os.Exit
 
+const CLRF = "\r\n"
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -19,6 +21,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	OKStatus := Status("200 OK")
+	NotFoundStatus := Status("404 Not Found")
+	PlainTextContent := ContentType("text/plain")
+
+	// for {
 	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
@@ -35,11 +42,36 @@ func main() {
 	req := strings.Fields(line)
 	target := req[1]
 
-	if target == "/" {
-		response := "HTTP/1.1 200 OK\r\n\r\n"
+	paths := strings.Split(target, "/")
+
+	switch {
+	case target == "/":
+		response := OKStatus + ContentLength(0) + CLRF
 		c.Write([]byte(response))
-	} else {
-		response := "HTTP/1.1 404 Not Found\r\n\r\n"
+	case paths[0] == "" && paths[1] == "echo" && len(paths) == 3:
+		pathParam := paths[2]
+		response := OKStatus +
+			PlainTextContent +
+			ContentLength(len(pathParam)) +
+			CLRF + pathParam
+		c.Write([]byte(response))
+	default:
+		response := NotFoundStatus + ContentLength(0) + CLRF
 		c.Write([]byte(response))
 	}
+
+	// 	c.Close()
+	// }
+}
+
+func Status(status string) string {
+	return fmt.Sprintf("HTTP/1.1 %s%s", status, CLRF)
+}
+
+func ContentType(ctype string) string {
+	return fmt.Sprintf("Content-Type: %s%s", ctype, CLRF)
+}
+
+func ContentLength(length int) string {
+	return fmt.Sprintf("Content-Length: %v%s", length, CLRF)
 }
